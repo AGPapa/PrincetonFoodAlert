bba311ea662e9dbf238301c5b89795cf112ef044
thomashikaru <thclark@princeton.edu>
2017-04-11 22:41:43 -0400

    Update foodalert.py

--- foodalert.py
+++ foodalert.py
@@ -55,19 +55,23 @@ def add_entry():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-     error = None
-     if request.method == 'POST':
-        C = CASClient.CASClient()
-        netid = C.authenticate
-         # if request.form['username'] != app.config['USERNAME']:
-         #     error = 'Invalid username'
-         # elif request.form['password'] != app.config['PASSWORD']:
-         #     error = 'Invalid password'
-         # else:
-         #     session['logged_in'] = True
-         #     flash('You were logged in')
-         #     return redirect(url_for('show_entries'))
-     return render_template('login.html', error=error)
+    error = None
+    # netid = C.Authenticate()
+    # session['logged_in'] = True
+    # print("BLABLABLA")
+
+    # if request.method == 'POST':
+    #  if request.form['username'] != app.config['USERNAME']:
+    #      error = 'Invalid username'
+    #  elif request.form['password'] != app.config['PASSWORD']:
+    #      error = 'Invalid password'
+    #  else:
+    #      session['logged_in'] = True
+    #      flash('You were logged in')
+    #      return redirect(url_for('show_entries'))
+    # return render_template('login.html', error=error)
+    session['logged_in'] = True
+    return redirect(url_for('show_entries'))
 
 @app.route('/logout')
 def logout():
