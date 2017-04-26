d7d99511a929d919b10f93e1b560b25c7d148574
Antonio G Papa <antoniogpapa@gmail.com>
2017-04-25 22:24:03 -0400

    Minor email fixes

--- email_composer.py
+++ email_composer.py
@@ -1,15 +1,15 @@
 import sys
 
 def write_email(netid, foodprefs, dhalls, dates, meals, foods):
-	sys.stdout.write("\n<email for " + netid + ">\n")
+	sys.stdout.write("<" + netid + ">\n")
 	count = 0
 	for food in foodprefs:
 		count = count + 1
 	sys.stdout.write("Hello " + netid + ", you have " + str(count) + " food(s) in the dining halls in the next week." + "\n")
 	for i in range(0, count):
-		sys.stdout.write("On " + dates[i] + " there will be " + foods[i][:-1] + " in " + dhalls[i] + " for " + meals[i] + ". You recieved this notification because you wanted emails for \"" + foodprefs[i] + "\".\n")
+		sys.stdout.write("There will be " + foods[i][:-2] + " in " + dhalls[i] + " for " + meals[i] + " on " + dates[i] + ". You received this notification because you wanted emails for \"" + foodprefs[i] + "\".\n")
 	sys.stdout.write("Thank you for using Princeton Food Alert. Please visit www.ptonfoodalert.herokuapp.com to update your preferences.\n")
-	sys.stdout.write("<end email>\n")
+	sys.stdout.write("<end of email>\n\n")
 
 
 foodprefs = []
