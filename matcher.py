0f4f99bb6969c4ed3fc1f7990a364ea01e9e993a
Antonio G Papa <antoniogpapa@gmail.com>
2017-05-10 23:43:03 -0400

    Checks account pref

--- matcher.py
+++ matcher.py
@@ -3,15 +3,16 @@ import sys
 import pymongo
 import json
 import datetime
+import time
 
-def check_date(dhall_line):
+def check_date(dhall_line, day_before, week_before):
 	tokens = dhall_line.split('\t')
 	
 	now = datetime.datetime.now();
 	dateText = tokens[0]
 	date = datetime.datetime(now.year, int(dateText[:2]), int(dateText[3:]), now.hour, now.minute, 0)
 	delta = date - now;
-	return (delta.days == -1 or delta.days == 5) #off by one, idk
+	return ((day_before and delta.days == -1) or (week_before and delta.days == 5)) #off by one, idk
 
 def match(food, dhall_line):
 	tokens = dhall_line.split('\t')
@@ -40,8 +41,18 @@ while True:
 	s = sys.stdin.readline()
 	if not s: break
 	for user in all:
-		if (check_date(s)):
-			for food in user['foodpref']:
-				if match(food.lower(), s):
-					sys.stdout.write(user['netid'] +  "\t" + food + "\t" + s)
+		start = time.time();
+		try:
+			day_before = 'd' in user['accountpref']
+			week_before = 'w' in user['accountpref']
+		except:
+			day_before = True
+			week_before = True
+		try:	
+			if (check_date(s, day_before, week_before)):
+				for food in user['foodpref']:
+					if match(food.lower(), s):
+						sys.stdout.write(user['netid'] +  "\t" + food + "\t" + s)
+		except KeyError:
+			pass
 	all.rewind()
