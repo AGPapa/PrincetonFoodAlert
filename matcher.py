a1188710e0040ed564169349052585eceb4a3045
Antonio G Papa <antoniogpapa@gmail.com>
2017-05-14 14:00:08 -0400

    Check dining hall preferences

--- matcher.py
+++ matcher.py
@@ -5,9 +5,22 @@ import json
 import datetime
 import time
 
-def check_date(dhall_line, day_before, week_before):
+def check_date(dhall_line, day_before, week_before, dhall_pref):
 	tokens = dhall_line.split('\t')
 	
+	if ('b' not in dhall_pref and tokens[2] == "Butler/Wilson"):
+		return False
+	elif ('r' not in dhall_pref and tokens[2] == "Rocky/Mathey"):
+		return False
+	elif ('f' not in dhall_pref and tokens[2] == "Forbes"):
+		return False
+	elif ('w' not in dhall_pref and tokens[2] == "Whitman"):
+		return False
+	elif ('c' not in dhall_pref and tokens[2] == "the Center For Jewish Life"):
+		return False
+	elif ('g' not in dhall_pref and tokens[2] == "the Graduate College"):
+		return False
+
 	now = datetime.datetime.now();
 	dateText = tokens[0]
 	date = datetime.datetime(now.year, int(dateText[:2]), int(dateText[3:]), now.hour, now.minute, 0)
@@ -48,8 +61,12 @@ while True:
 		except:
 			day_before = True
 			week_before = True
+		try:
+			dhall_pref = user['dhallpref']
+		except:
+			dhall_pref = 'brfwcg'
 		try:	
-			if (check_date(s, day_before, week_before)):
+			if (check_date(s, day_before, week_before, dhall_pref)):
 				for food in user['foodpref']:
 					if match(food.lower(), s):
 						sys.stdout.write(user['netid'] +  "\t" + food + "\t" + s)
--- scrape_match_email.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python scraper.py > scraped_output.txt
-python matcher.py < scraped_output.txt > match_output.txt
-sort < match_output.txt > sort_output.txt
-python email_composer.py < sort_output.txt > emails.txt
